8f6f1027afcddf917cae9790429f05c12e52c47d
DESKTOP-RS6QGBT\Nelson <nelsonunes13@gmail.com>
2025-08-06 19:06:06 +0100

    first commit;

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
             <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Master Cleanse Reliac
               Heirloom</h1>
             <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn
-              asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun
+              asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven&#39;t heard of them man bun
               deep jianbing selfies heirloom.</p>
           </div>
           <div className="flex flex-wrap -m-4">
